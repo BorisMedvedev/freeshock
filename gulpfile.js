8f7066557f67fc56879d237f1124fe2c686a7e2e
Your Name <you@example.com>
2022-04-24 20:16:59 +0300

    add all

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
 const { src, dest, watch, parallel, series } = require('gulp');
 
-const scss            = require('gulp-sass')(require('sass'));
-const concat          = require('gulp-concat');
-const autoprefixer    = require('gulp-autoprefixer');
-const uglify          = require('gulp-uglify');
-const image           = require('gulp-image');
-const del             = require('del');
-const browserSync     = require('browser-sync').create();
-const svgSprite       = require('gulp-svg-sprite');
-const cheerio         = require('gulp-cheerio');
-const replace         = require('gulp-replace');
-const fileInclude     = require('gulp-file-include');
+const scss = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
+const uglify = require('gulp-uglify');
+
+const del = require('del');
+const browserSync = require('browser-sync').create();
+const svgSprite = require('gulp-svg-sprite');
+const cheerio = require('gulp-cheerio');
+const replace = require('gulp-replace');
+const fileInclude = require('gulp-file-include');
 
 const htmlInclude = () => {
   return src(['app/html/*.html']) // Находит любой .html файл в папке "html", куда будем подключать другие .html файлы													
@@ -22,18 +22,19 @@ const htmlInclude = () => {
     .pipe(browserSync.stream());
 }
 
+
 function svgSprites() {
   return src('app/images/icons/*.svg')
-  .pipe(cheerio({
-    run: ($) => {
+    .pipe(cheerio({
+      run: ($) => {
         $("[fill]").removeAttr("fill");
-        $("[stroke]").removeAttr("stroke"); 
-        $("[style]").removeAttr("style"); 
-    },
-    parserOptions: { xmlMode: true },
-  })
-)
-    .pipe(replace('&gt;','>'))
+        $("[stroke]").removeAttr("stroke");
+        $("[style]").removeAttr("style");
+      },
+      parserOptions: { xmlMode: true },
+    })
+    )
+    .pipe(replace('&gt;', '>'))
     .pipe(
       svgSprite({
         mode: {
@@ -43,8 +44,8 @@ function svgSprites() {
         },
       })
     )
-		.pipe(dest('app/images'));
-  }
+    .pipe(dest('app/images'));
+}
 
 function browsersync() {
   browserSync.init({
@@ -57,7 +58,7 @@ function browsersync() {
 
 function styles() {
   return src('app/scss/style.scss')
-    .pipe(scss({outputStyle: 'compressed'}))
+    .pipe(scss({ outputStyle: 'compressed' }))
     .pipe(concat('style.min.css'))
     .pipe(autoprefixer({
       overrideBrowserslist: ['last 10 versions'],
@@ -65,7 +66,7 @@ function styles() {
     }))
     .pipe(dest('app/css'))
     .pipe(browserSync.stream())
-} 
+}
 
 function scripts() {
   return src([
@@ -80,24 +81,24 @@ function scripts() {
     'node_modules/rateyo/src/jquery.rateyo.js',
     'app/js/main.js'
   ])
-  .pipe(concat('main.min.js'))
-  .pipe(uglify())
-  .pipe(dest('app/js'))
-  .pipe(browserSync.stream())
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(dest('app/js'))
+    .pipe(browserSync.stream())
 }
 
-function images (){
+function images() {
   return src('app/images/**/*.*')
-  .pipe(image({
-  optipng: ['-i 1', '-strip all', '-fix', '-o7', '-force'],
-  pngquant: ['--speed=1', '--force', 256],
-  zopflipng: ['-y', '--lossy_8bit', '--lossy_transparent'],
-  jpegRecompress: ['--strip', '--quality', 'medium', '--min', 40, '--max', 80],
-  mozjpeg: ['-optimize', '-progressive'],
-  gifsicle: ['--optimize'],
-  svgo: ['--enable', 'cleanupIDs', '--disable', 'convertColors']
-}))
-  .pipe(dest('dist/images'))
+    .pipe(image({
+      optipng: ['-i 1', '-strip all', '-fix', '-o7', '-force'],
+      pngquant: ['--speed=1', '--force', 256],
+      zopflipng: ['-y', '--lossy_8bit', '--lossy_transparent'],
+      jpegRecompress: ['--strip', '--quality', 'medium', '--min', 40, '--max', 80],
+      mozjpeg: ['-optimize', '-progressive'],
+      gifsicle: ['--optimize'],
+      svgo: ['--enable', 'cleanupIDs', '--disable', 'convertColors']
+    }))
+    .pipe(dest('dist/images'))
 }
 
 function build() {
@@ -105,8 +106,8 @@ function build() {
     'app/**/*.html',
     'app/css/style.min.css',
     'app/js/main.min.js',
-  ],{base: 'app'})
-  .pipe(dest('dist'))
+  ], { base: 'app' })
+    .pipe(dest('dist'))
 }
 
 function cleanDist() {
@@ -121,13 +122,13 @@ function watching() {
   watch(['app/html/**/*.html'], htmlInclude);
 }
 
-exports.styles      = styles;
-exports.svgSprites  = svgSprites;
-exports.scripts     = scripts;
+exports.styles = styles;
+exports.svgSprites = svgSprites;
+exports.scripts = scripts;
 exports.browsersync = browsersync;
-exports.watching    = watching;
-exports.images      = images;
-exports.cleanDist   = cleanDist;
-exports.build       = series(cleanDist, images, build);
+exports.watching = watching;
+exports.images = images;
+exports.cleanDist = cleanDist;
+exports.build = series(cleanDist, images, build);
 exports.htmlInclude = htmlInclude;
-exports.default = parallel(htmlInclude, styles, svgSprites, scripts, browsersync, watching, );
+exports.default = parallel(htmlInclude, styles, svgSprites, scripts, browsersync, watching,);
